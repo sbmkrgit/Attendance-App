f9f2ab59815446f565cacfb756f80eface96b288
Shubham <babukumar9931@gmail.com>
2020-11-08 16:10:34 +0530

    Added Mark Attendance Page

---
+++ lib/pages/MarkAttendance.dart
@@ -0,0 +1,124 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MarkAttendance extends StatefulWidget {
+  @override
+  _MarkAttendanceState createState() => _MarkAttendanceState();
+}
+
+class _MarkAttendanceState extends State<MarkAttendance> {
+  final db = Firestore.instance;
+  String status = "Absent";
+
+  Future<void> attendanceMark() async {
+    await db.collection("students").add({
+      'attendance': "present",
+    }).then((documentReference) {
+      print(documentReference.documentID);
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  /* generateStudentList(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return snapshot.data.documents
+        .map<Widget>(
+          (doc) => new ListTile(
+              onTap: () {
+                //to be implemented
+              },
+              title: new Text(
+                doc["rollNo"].toString() +
+                    ". " +
+                    doc["name"].toString().toUpperCase(),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              trailing: Checkbox(
+                  value: false,
+                  onChanged: (bool value) {
+                    present = value;
+                  })),
+        )
+        .toList();
+  } */
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Mark Attendance"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: db.collection('students').orderBy('rollNo').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot ds = snapshot.data.documents[index];
+                        return Container(
+                          /* decoration: new BoxDecoration(
+                            color: Colors.red
+                          ), */
+                          child: ListTile(
+                            title: Text(
+                              ds['rollNo'].toString() +
+                                  ". " +
+                                  ds['name'].toString().toUpperCase(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                            onTap: () {
+                              if (ds['attendance'].toString() == "Absent") {
+                                db
+                                    .collection("students")
+                                    .document(ds.documentID)
+                                    .updateData({"attendance": "Present"});
+                              } else {
+                                db
+                                    .collection("students")
+                                    .document(ds.documentID)
+                                    .updateData({"attendance": "Absent"});
+                              }
+                            },
+                            leading: Icon(Icons.person, color: Colors.black),
+                            trailing: Text(ds['attendance']
+                                .toString()), /* Checkbox(
+                                value: present,
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    present = value;
+                                  });
+                                }), */
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return CircularProgressIndicator();
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+            ),
+            Divider(),
+            Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: RaisedButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("SUBMIT"),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
